ed5b54f97dad2e269e7769a9f29eb8ae102b4652
vvh0ame <77536370+vvh0ame@users.noreply.github.com>
2024-10-09 04:00:34 +0500

    Update world_noor.py

--- src/world_noor.py
+++ src/world_noor.py
@@ -1,276 +1,175 @@
-import requests
-from time import time
-from uuid import uuid4
-
-class WorldNoor:
-	def __init__(self, device_type: str = "android") -> None:
-		self.api = "https://worldnoor.com/api"
-		self.node_api = "https://nodeapi.worldnoor.com/api"
-		self.headers = {
+class WorldNoor {
+	constructor() {
+		this.api = "https://worldnoor.com/api"
+		this.nodeApi = "https://nodeapi.worldnoor.com/api"
+		this.headers = {
 			"user-agent": "okhttp/4.9.1",
-			"authorization": f"Bearer {uuid4()}"
+			"authorization": `Bearer ${uuid4()}`
 		}
-		self.token = None
-		self.user_id = None
-		self.jwt_token = None
-		self.device_type = device_type
+		this.deviceType = "android"
+	}
 
-	def register(
-			self,
-			name: str,
-			email: str,
-			password: str,
-			country_code: str = "US") -> dict:
-		return requests.post(
-			f"{self.api}/register_new?password={password}&device_type={self.device_type}&country_code={country_code}&&email={email}&password_confirmation={password}&firstname={name}",
-			headers=self.headers).json()
+	async register(name, email, password, countryCode = "US") {
+		const response = await fetch(
+			`${this.api}/register_new?password=${password}&deviceType=${this.deviceType}&country_code=${countryCode}&&email=${email}&password_confirmation=${password}&firstname=${name}`, {
+				method: "POST",
+				headers: this.headers
+			})
+		return response.json()
+	}
 
-	def login(
-			self,
-			email: str,
-			password: str) -> dict:
-		response = requests.post(
-			f"{self.api}/login?email={email}&password={password}&device_type={self.device_type}",
-			headers=self.headers).json()
-		if "token" in response["data"]:
-			self.token = response["data"]["token"]
-			self.jwt_token = response["data"]["jwtToken"]
-			self.user_id = self.verify_login()["data"]["id"]
-			self.headers["jwttoken"] = self.jwt_token
-		return response
-
-	def verify_login(self) -> dict:
-		return requests.post(
-			f"{self.api}/login-verify?skip_verification=1&token={self.token}",
-			headers=self.headers).json()
-
-	def get_all_notifications(self) -> dict:
-		return requests.get(
-			f"{self.api}/notifications/get_all?token={self.token}",
-			headers=self.headers).json()
-
-	def get_news_feed(self, page: int = 1) -> dict:
-		return requests.get(
-			f"{self.api}/newsfeed?page={page}&token={self.token}",
-			headers=self.headers).json()
-
-	def get_languages(self) -> dict:
-		return requests.get(
-			f"{self.api}/meta/languages?token={self.token}",
-			headers=self.headers).json()
-
-	def get_stories(self) -> dict:
-		return requests.post(
-			f"{self.api}/stories?token={self.token}",
-			headers=self.headers).json()
-
-	def get_reels(
-			self,
-			page: int = 1,
-			fetch_public_posts_only: bool = True) -> dict:
-		return requests.get(
-			f"{self.api}/newsfeed-videos?page={page}&fetch_public_posts_only={fetch_public_posts_only}&token={self.token}",
-			headers=self.headers).json()
-
-	def get_account_basic(self) -> dict:
-		return requests.get(
-			f"{self.api}/profile/basic?token={self.token}",
-			headers=self.headers).json()
-
-	def react_to_post(
-			self,
-			post_id: int,
-			reaction_type: str = "like") -> dict:
-		return requests.post(
-			f"{self.api}/react?post_id={post_id}&type={reaction_type}&token={self.token}",
-			headers=self.headers).json()
-
-
-	def comment_post(
-			self,
-			post_id: int,
-			body: str) -> dict:
-		data = {
-			"post_id": post_id,
-			"body": body,
-			"identifier": int(time() * 1000),
-			"token": self.token
+	async login(email, password) {
+		const response = await fetch(
+			`${this.api}/login?email=${email}&password=${password}&deviceType=${this.deviceType}`, {
+				method: "POST",
+				headers: this.headers
+			})
+		const data = await response.json()
+		if ("token" in data) {
+			this.token = data.token
+			this.jwtToken = data.jwtToken
+			this.headers["jwttoken"] = this.jwtToken
+			this.userId = await this.verifyLogin().data.id
 		}
-		return requests.post(
-			f"{self.api}/comment",
-			data=data,
-			headers=self.headers).json() # 1669315224824
+	}
 
-	def edit_comment(
-			self,
-			post_id: int,
-			body: str,
-			comment_id: int) -> dict:
-		data = {
-			"post_id": post_id,
-			"body": body,
-			"identifier": int(time() * 1000),
-			"token": self.token,
-			"comment_id": comment_id
-		}
-		return requests.post(
-			f"{self.api}/comment",
-			data=data,
-			headers=self.headers).json()
+	async verifyLogin(email, password, verificationCode) {
+		const response = await fetch(
+			`${this.api}/login-verify?skip_verification=1&token=${this.token}`, {
+				method: "POST",
+				headers: this.headers
+			})
+		return response.json()
+	}
 
-	def delete_comment(
-			self,
-			comment_id: int) -> dict:
-		return requests.delete(
-			f"{self.api}/comment?token={self.token}&comment_id={comment_id}",
-			headers=self.headers).json()
+	async getNotifications() {
+		const response = await fetch(
+			`${this.api}/notifications/get_all?token=${this.token}`, {
+				method: "GET",
+				headers: this.headers
+			})
+		return response.json()
+	}
 
-	def get_post_info(self, post_id: int) -> dict:
-		return requests.get(
-			f"{self.api}/post/get-single-newsfeed-item/{post_id}?token={self.token}",
-			headers=self.headers).json()
+	async getNewsFeed(page = 1) {
+		const response = await fetch(
+			`${this.api}/newsfeed?page=${page}&token=${this.token}`, {
+				method: "GET",
+				headers: this.headers
+			})
+		return response.json()
+	}
 
-	def report_post(
-			self,
-			post_id: int,
-			report_id: int,
-			body: str = None) -> dict:
-		"""
-		REPORT-IDS:
-			1 - SPAM,
-			2 - VIOLENCE,
-			3 - HARASSMENT,
-			4 - FALSE NEWS,
-			5 - HATE SPEECH,
-			6 - TERRORISM,
-			7 - SUICIDE,
-			8 - NUDITY,
-			9 - SOMETHING ELSE
-		"""
-		url = f"{self.api}/post/report?report_id={report_id}&token={self.token}&report_post_id={post_id}"
-		if body:
-			url += f"&body={body}"
-		return requests.post(
-			url, headers=self.headers).json()
+	async getStories() {
+		const response = await fetch(
+			`${this.api}/stories?token=${this.token}`, {
+				method: "POST",
+				headers: this.headers
+			})
+		return response.json()
+	}
 
-	def get_user_profile(self, user_id: int) -> dict:
-		return requests.get(
-			f"{self.api}/profile/about?token={self.token}&user_id={user_id}",
-			headers=self.headers).json()
+	async getReels(page = 1, fetchPublicPostsOnly = true) {
+		const response = await fetch(
+			`${this.api}/newsfeed-videos?page=${page}&fetch_public_posts_only=${fetchPublicPostsOnly}&token=${this.token}`, {
+				method: "GET",
+				headers: this.headers
+			})
+		return response.json()
+	}
 
-	def send_friend_request(
-			self,
-			user_id: int) -> dict:
-		return requests.post(
-			f"{self.api}/user/send_friend_request?token={self.token}&user_id={user_id}",
-			headers=self.headers).json()
+	async getAccountBasic() {
+		const response = await fetch(
+			`${this.api}/profile/basic?token=${this.token}`, {
+				method: "GET",
+				headers: this.headers
+			})
+		return response.json()
+	}
 
-	def cancel_friend_request(
-			self,
-			user_id: int) -> dict:
-		return requests.post(
-			f"{self.api}/user/cancel_friend_request?token={self.token}&user_id={user_id}",
-			headers=self.headers).json()
+	async reactToPost(postId, reactionType) {
+		const response = await fetch(
+			`${this.api}/react?post_id=${postId}&type=${reactionType}&token=${this.token}`, {
+				method: "POST",
+				headers: this.headers
+			})
+		return response.json()
+	}
 
-	def get_contacts(self) -> dict:
-		return requests.get(
-			f"{self.api}/contacts?token={self.token}",
-			headers=self.headers).json()
+	async commentPost(postId, body) {
+		const response = await fetch(
+			`${this.api}/comment`, {
+				method: "POST",
+				body: JSON.stringify({
+					post_id: postId,
+					body: body,
+					identifier: Math.floor(Date.now() / 1000) * 1000,
+					token: this.token
+				}),
+				headers: this.headers
+			})
+		return response.json()
+	}
 
-	def get_friends(self) -> dict:
-		return requests.get(
-			f"{self.api}/user/friends?token={self.token}",
-			headers=self.headers).json()
 
-	def get_generic_notifications(self) -> dict:
-		data = {
-			"is_mobile": is_mobile,
-			"token": self.token
-		}
-		return requests.post(
-			f"{self.node_api}/generic_notifications",
-			data=data,
-			headers=self.headers).json()
+	async getPostInfo(postId) {
+		const response = await fetch(
+			`${this.api}/post/get-single-newsfeed-item/${postId}?token=${this.token}`, {
+				method: "GET",
+				headers: this.headers
+			})
+		return response.json()
+	}
 
-	def get_conversations(
-			self,
-			page: int = 1,
-			per_page: int = 10) -> dict:
-		data = {
-			"page": page,
-			"per_page": per_page,
-			"token": self.token
-		}
-		return requests.post(
-			f"{self.node_api}/conversations",
-			data=data,
-			headers=self.headers).json()
+	async getUserInfo(userId) {
+		const response = await fetch(
+			`${this.api}/profile/about?token=${this.token}&user_id=${userId}`, {
+				method: "GET",
+				headers: this.headers
+			})
+		return response.json()
+	}
 
-	def get_images(self) -> dict:
-		return requests.get(
-			F"{self.api}/images/by_sections?token={self.token}",
-			headers=self.headers).json()
+	async sendFriendRequest(userId) {
+		const response = await fetch(
+			`${this.api}/user/send_friend_request?token=${this.token}&user_id=${userId}`, {
+				method: "GET",
+				headers: this.headers
+			})
+		return response.json()
+	}
 
-	def get_saved_posts(self) -> dict:
-		return requests.get(
-			f"{self.api}/saved/newsfeed?token={self.token}",
-			headers=self.headers).json()
+	async cancelFriendRequest(userId) {
+		const response = await fetch(
+			`${this.api}/user/cancel_friend_request?token=${this.token}&user_id=${userId}`, {
+				method: "GET",
+				headers: this.headers
+			})
+		return response.json()
+	}
 
-	def get_pages(self) -> dict:
-		return requests.get(
-			f"{self.api}/page/list?token={self.token}",
-			headers=self.headers).json()
+	async getFriends() {
+		const response = await fetch(
+			`${this.api}/user/friends?token=${this.token}`, {
+				method: "POST",
+				headers: this.headers
+			})
+		return response.json()
+	}
 
-	def get_page_categories(self) -> dict:
-		return requests.get(
-			f"{self.api}/page/categories_meta?token={self.token}",
-			headers=self.headers).json()
+	async getConversations(page = 1, perPage = 10) {
+		const response = await fetch(
+			`${this.nodeApi}/conversations`, {
+				method: "POST",
+				body: JSON.stringify({
+					page: page,
+					per_page: perPage,
+					token: this.token
+				}),
+				headers: this.headers
+			})
+		return response.json()
+	}
+}
 
-	def create_page(
-			self,
-			title: str,
-			category_id: int,
-			page_url: str) -> dict:
-		return requests.post(
-			f"{self.api}/page/create?page_title={title}&page_category_ids[]={category_id}&token={self.token}&page_url={page_url}",
-			headers=self.headers).json()
-
-	def get_groups(self) -> dict:
-		return requests.get(
-			f"{self.api}/group/list?token={self.token}",
-			headers=self.headers).json()
-
-	def get_town_hall(self) -> dict:
-		return requests.get(
-			f"{self.api}/town_hall/get_data?token={self.token}",
-			headers=self.headers).json()
-
-	def update_password(
-			self,
-			old_password: str,
-			new_password: str) -> dict:
-		return requests.post(
-			f"{self.api}/profile/update_password?token={self.token}&new_password={new_password}&current_password={old_password}&new_password_confirmation={new_password}",
-			headers=self.headers).json()
-
-	def get_login_sessions(self) -> dict:
-		return requests.get(
-			f"{self.api}/profile/login_sessions?token={self.token}",
-			headers=self.headers).json()
-
-	def delete_login_session(
-			self,
-			session_id: int) -> dict:
-		return requests.post(
-			f"{self.api}/profile/delete_login_session?session_id={session_id}&token={self.token}",
-			headers=self.headers).json()
-
-	def get_hidden_posts(self) -> dict:
-		return requests.get(
-			f"{self.api}/hidden/newsfeed?token={self.token}",
-			headers=self.headers).json()
-
-	def get_blocked_users(self) -> dict:
-		return requests.get(
-			f"{self.api}/user/blocked_users?token={self.token}",
-			headers=self.headers).json()
+module.exports = {WorldNoor}
